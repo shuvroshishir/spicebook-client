73fa1161ab1a1b01eb5f7dc7e9df692df41f8a2f
shuvroshishir <shuvroshishirx@gmail.com>
2026-06-21 23:43:10 +0600

    first commit

---
+++ src/app/layout.jsx
@@ -0,0 +1,36 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "SpiceBook - A Recipe Sharing Platform",
+  description: "SpiceBook is a recipe sharing platform where users can share and discover delicious recipes from around the world. Join our community and explore a variety of culinary delights!",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html
+      lang="en"
+      suppressHydrationWarning
+      className={`${geistSans.className} ${geistSans.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">
+        <header className="sticky top-0 z-50">
+
+        </header>
+        <main className="min-h-screen bg-background text-foreground flex-1">
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
